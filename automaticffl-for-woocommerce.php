e0a92db6a3cb0d489df18a6d119466e7572c5a5f
Tiago Rodrigues <tiagor@razoyo.com>
2023-08-31 14:23:51 -0300

    Display Version Information on AutomaticFFL Settings Screen

--- automaticffl-for-woocommerce.php
+++ automaticffl-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: The official Automatic FFL for WooCommerce plugin
  * Author: Refactored Group
  * Author URI: http://refactored.group
- * Version: 1.0.0
+ * Version: 1.0.3
  * Text Domain: automaticffl-for-woocommerce
  * Domain Path: /i18n/languages/
  *
